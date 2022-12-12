2717112f326ab3b82e352342aee5e9a5c58800f0
AnActualBanana <92802843+AnActualBanana@users.noreply.github.com>
2022-12-12 09:08:52 -0600

    Create 3-hbnb.js
    Still need to make the looping function for Places

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    const amenities_checked = {};
+    $(document).on('change', "input[type='checkbox']", function() {
+        if (this.checked) {
+            amenities_checked[$(this).data('id')] = $(this).data('name');
+
+        } else {
+            delete amenities_checked[$(this).data('id')];
+        }
+        const amenities_list = []
+        for (const idx in amenities_checked) {
+            amenities_list.push(amenities_checked[idx]);
+        }
+        $('.amenities h4').text(amenities_list.join(', '));
+    });
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        }
+    });
+  
+});
