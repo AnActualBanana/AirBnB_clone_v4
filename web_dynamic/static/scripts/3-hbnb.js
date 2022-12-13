439c6c68bd637765dcd1cbc4b3e60647b8928df2
AnActualBanana <92802843+AnActualBanana@users.noreply.github.com>
2022-12-12 18:45:17 -0600

    Update 3-hbnb.js
    I don't know how to test this, this is my best guess.

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -20,5 +20,47 @@ $(document).ready(function() {
           $('#api_status').removeClass('available');
         }
     });
-  
+    $.ajax({
+      url: url,
+      type: 'POST',
+      contentType: 'application/json', 
+      dataType: 'json',
+      data: JSON.stringify({}),
+      success: function(data) {
+        for (const place of data) {
+          $.get('http://0.0.0.0:5001/api/v1/places_search/' + place.user_id, function (userData) {
+            let guestsPlural = '';
+            if (place.max_guest != 1) {
+              let guestsPlural ='s';
+            } 
+            let roomsPlural = ''
+            if (place.number_rooms != 1) {
+              let roomsPlural ='s';
+            }
+            let bathroomPlural = ''
+            if (place.number_bathrooms != 1) {
+              let bathroomPlural ='s';
+            } 
+            let html = `<article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${guestsPlural}</div>
+                    <div class="number_rooms">${place.number_rooms } Bedroom${roomsPlural}</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathroom${bathroomPlural}</div>
+            </div>
+            <div class="user">
+                    <b>Owner:</b> ${userData.first_name} ${userData.last_name}
+                  </div>
+                  <div class="description">
+              ${place.description}
+                  </div>
+              </article>`;
+          $('.places').append(html);
+          });
+        }
+      }
+    });
 });
